a539a76d30237fa2daae7665942bc7e296561698
shiba_1234 <shibajisahu121@gmail.com>
2023-02-21 22:31:10 +0530

    Some bug fixed and Read me file and comment added

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Getting Started with Stop Watch Web site
+
+This Website is designed by using HTML, CSS and [Vanilla Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+# To Run the Website
+
+Runs the app in development mode. \
+Open index.html and open with live server. Check the [live server](http://127.0.0.1:5500/index.html)
+
+# Production website of this app
+
+Open chrome and Go to this [Stop Watch URL](https://mystopwatch1008.netlify.app/)
+
+# functionalities of Stop watch
+
+The Timer screen contains `Minute : Second : MiliSecond`
+
+Three buttons present with one digital timer screen.\
+
+1. Start Button - Starts the timer.\
+2. Stop Button - Stops the timer.\
+3. Reset Button - Resets the timer screen.
--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-let timerInterval;
-let timer = false;
+let timerInterval; //Timer variable to use set and clear interval
+let timer = false; // a boolean to check timer is running or not
+
+// Declaring all the time elements
 let miliSecElement = document.getElementById("mili-second");
 let secElement = document.getElementById("second");
 let minElement = document.getElementById("min");
 
+// function to run the timer
 function runTimer() {
   let miliSec = parseInt(miliSecElement.innerText, 10);
   let sec = parseInt(secElement.innerText, 10);
   let min = parseInt(minElement.innerText, 10);
   miliSecElement.innerText = (miliSec + 1).toString().padStart(2, "0");
 
-  if (miliSec === 10) {
-    secElement.innerText = (sec + 1).toString().padStart(2, "0");
+  if (miliSec === 99) {
+    if (sec !== 59) {
+      secElement.innerText = (sec + 1).toString().padStart(2, "0");
+    }
     miliSecElement.innerText = "00";
   }
-  if (sec === 59) {
+  if (sec === 59 && miliSec === 99) {
     minElement.innerText = (min + 1).toString().padStart(2, "0");
     secElement.innerText = "00";
   }
 }
 
+// Function will execute after clicking Start button
 function startTimer() {
   if (!timer) {
-    timerInterval = setInterval(runTimer, 100);
+    timerInterval = setInterval(runTimer, 10);
     timer = true;
   }
 }
 
+// Function to be executed after clicking the stop button
 function stopTimer() {
   clearInterval(timerInterval);
   timer = false;
 }
 
+// Function to be executed after clicking the reset button
 function resetTimer() {
   clearInterval(timerInterval);
   miliSecElement.innerText = "00";
