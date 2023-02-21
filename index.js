48c8996586f0567e1ecc4ae7412fc1392d18b3b4
shiba_1234 <shibajisahu121@gmail.com>
2023-02-21 14:54:44 +0530

    milisecond added

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-let secTimer;
+let timerInterval;
 let timer = false;
+let miliSecElement = document.getElementById("mili-second");
 let secElement = document.getElementById("second");
 let minElement = document.getElementById("min");
 
 function runTimer() {
+  let miliSec = parseInt(miliSecElement.innerText, 10);
   let sec = parseInt(secElement.innerText, 10);
-  secElement.innerText = (sec + 1).toString().padStart(2, "0");
+  let min = parseInt(minElement.innerText, 10);
+  miliSecElement.innerText = (miliSec + 1).toString().padStart(2, "0");
+
+  if (miliSec === 10) {
+    secElement.innerText = (sec + 1).toString().padStart(2, "0");
+    miliSecElement.innerText = "00";
+  }
   if (sec === 59) {
-    let min = parseInt(minElement.innerText, 10);
     minElement.innerText = (min + 1).toString().padStart(2, "0");
     secElement.innerText = "00";
   }
@@ -15,18 +22,19 @@ function runTimer() {
 
 function startTimer() {
   if (!timer) {
-    secTimer = setInterval(runTimer, 1000);
+    timerInterval = setInterval(runTimer, 100);
     timer = true;
   }
 }
 
 function stopTimer() {
-  clearInterval(secTimer);
+  clearInterval(timerInterval);
   timer = false;
 }
 
 function resetTimer() {
-  clearInterval(secTimer);
+  clearInterval(timerInterval);
+  miliSecElement.innerText = "00";
   secElement.innerText = "00";
   minElement.innerText = "00";
   timer = false;
